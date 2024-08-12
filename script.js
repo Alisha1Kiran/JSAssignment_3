617cbb0aee5f35c21b74c0c5a33882d8629a84ce
Alisha Kiran <alishasatheesan1992@gmail.com>
2024-08-12 22:55:03 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function changelogo(){
+    let image = document.getElementById("logo");
+    image.src = "images/css.png";
+}
+
+function reset(){
+    let image = document.getElementById("logo");
+    image.src = "images/html.png";
+}
